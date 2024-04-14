f5f9d1809454b3820f62fd3687899c298896f128
Sophia Namaganda <sophia@kali.kali>
2024-04-14 05:26:20 -0500

    squares

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,24 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{n}")
+    //unimplemented!("square of sum of 1...{n}")
+    let mut numbers = 0;
+    for i in 0..n + 1 {
+        numbers += i;
+    }
+    return numbers.pow(2);
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{n}")
+    //unimplemented!("sum of squares of 1...{n}")
+        let mut numbers = vec![];
+    for i in 0..n + 1 {
+        numbers.push(i);
+    }
+
+    let sum: u32 = numbers.iter().fold(0, |acc, number| acc + number.pow(2));
+    return sum;
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    return square_of_sum(n) - sum_of_squares(n);
+    // unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}");
 }
